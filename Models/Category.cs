9074a1e9cc5529856bfd4a3c4443de9ffb942705
ThSoaresDev <thiagosoaresvasconcelos@outlook.com>
2025-01-08 08:45:41 -0300

    add entites product and category

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace CRUD_produtos.Models;
+
+public class Category
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public string? imageUrl { get; set; }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD_produtos.Models;
+public class Product
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public string? description { get; set; }
+    public decimal price { get; set; }
+    public decimal quantity { get; set; }
+    public string? imageUrl { get; set; }
+    public float stock {get; set;}
+    public DateTime registrationDate { get; set; }
+    
+}
+
